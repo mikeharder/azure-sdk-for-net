fbc38154e5b3d54ecf9b2f7b91d5952610bdcbf7
Mike Harder <mharder@microsoft.com>
2020-11-18 21:43:47 -0800

    Add empty perf test

---
+++ sdk/eventhub/Azure.Messaging.EventHubs.Processor/perf/EventProcessorClientTest.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Test.Perf;
+using CommandLine;
+
+namespace Azure.Template.Perf
+{
+    public class EventProcessorClientTest : PerfTest<EventProcessorClientTest.EventProcessorClientTestOptions>
+    {
+        public EventProcessorClientTest(EventProcessorClientTestOptions options) : base(options)
+        {
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+        }
+
+        public override async Task RunAsync(CancellationToken cancellationToken)
+        {
+            await Task.CompletedTask;
+        }
+
+        public class EventProcessorClientTestOptions : PerfOptions
+        {
+        }
+    }
+}
