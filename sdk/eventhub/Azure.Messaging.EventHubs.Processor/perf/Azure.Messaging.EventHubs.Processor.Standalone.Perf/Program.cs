26df13f03ae641d0dff0abb9ccccc709bc44ffaf
Mike Harder <mharder@microsoft.com>
2020-11-18 21:53:53 -0800

    Add standalone perf project and solution

---
+++ sdk/eventhub/Azure.Messaging.EventHubs.Processor/perf/Azure.Messaging.EventHubs.Processor.Standalone.Perf/Program.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Azure.Template.Perf
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("EVENT_HUBS_CONNECTION_STRING");
+            var eventHubName = Environment.GetEnvironmentVariable("EVENT_HUB_NAME");
+        }
+    }
+}
