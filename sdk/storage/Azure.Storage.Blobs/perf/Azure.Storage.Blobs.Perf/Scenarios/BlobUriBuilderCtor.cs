9c1041090a2d1b5fee28d164923baaa950439b60
Mike Harder <mharder@microsoft.com>
2021-02-01 20:56:17 -0800

    Add simple client-only test

---
+++ sdk/storage/Azure.Storage.Blobs/perf/Azure.Storage.Blobs.Perf/Scenarios/BlobUriBuilderCtor.cs
@@ -0,0 +1,35 @@
+﻿//Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Test.Perf;
+
+namespace Azure.Storage.Blobs.Perf.Scenarios
+{
+    /// <summary>
+    /// The performance test scenario focused on creating a BlobUriBuilder.
+    /// </summary>
+    /// <seealso cref="Azure.Test.Perf.PerfTest{PerfOptions}" />
+    public sealed class BlobUriBuilderCtor : PerfTest<PerfOptions>
+    {
+        private static readonly Uri s_uri = new Uri("https://myaccount.blob.core.windows.net/mycontainer/myblob");
+
+        public BlobUriBuilderCtor(PerfOptions options) : base(options)
+        {
+        }
+
+        public override void Run(CancellationToken cancellationToken)
+        {
+            _ = new BlobUriBuilder(s_uri);
+        }
+
+        public override Task RunAsync(CancellationToken cancellationToken)
+        {
+            Run(cancellationToken);
+            return Task.CompletedTask;
+        }
+    }
+}
